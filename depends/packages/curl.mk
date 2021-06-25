6589391749d881538be70e2bd5b979b4a29617f5
Matthieu Derasse <matthieu@derasse.fr>
2021-06-25 21:59:49 +0000

    Upgrade build dependencies

--- depends/packages/curl.mk
+++ depends/packages/curl.mk
@@ -1,20 +1,21 @@
 package=curl
-$(package)_version=7.62.0
+$(package)_version=7.77.0
 $(package)_download_path=https://curl.haxx.se/download/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=55ccd5b5209f8cc53d4250e2a9fd87e6f67dd323ae8bd7d06b072cfcbb7836cb
+$(package)_sha256_hash=b0a3428acb60fa59044c4d0baae4e4fc09ae9af1d8a3aa84b2e3fbcd99841f77
 $(package)_dependencies=openssl zlib
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --with-ca-fallback
-$(package)_config_opts_mingw32=--enable-sspi --without-ssl --with-winssl --with-schannel
-$(package)_config_opts_darwin=--enable-sspi --without-ssl --with-darwinssl --with-secure-transport
+$(package)_config_opts_mingw32=--enable-sspi --without-ssl --with-nss --with-schannel
+$(package)_config_opts_darwin=--enable-sspi --without-ssl --with-secure-transport
+$(package)_config_opts_linux=--with-openssl
 $(package)_cflags=-DCURL_STATICLIB
 $(package)_cxxflags=-std=c++11
 endef
 
 define $(package)_config_cmds
-  ./buildconf && \
+  autoreconf -fi && \
   $($(package)_autoconf)
 endef
 
--- depends/packages/minizip.mk
+++ depends/packages/minizip.mk
@@ -8,14 +8,6 @@ $(package)_dependencies=zlib
 define $(package)_set_vars
 $(package)_config_opts=--enable-cxx --disable-shared --enable-static
 $(package)_config_opts_linux=--with-pic
-
-ifeq ($(host),x86_64-apple-darwin16)
-$(package)_cc=$(BASEDIR)/$(host)/native/bin/clang
-$(package)_cxx=$(BASEDIR)/$(host)/native/bin/clang++
-$(package)_cxxflags=-fPIC
-$(package)_cflags=-fPIC
-$(package)_cppflags=-fPIC
-endif
 endef
 
 define $(package)_config_cmds
