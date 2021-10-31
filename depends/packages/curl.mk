76e3e3fa9df92f6d6da3aeb7de3ca141916ce6b4
Matthieu 'JP' DERASSE <github@derasse.fr>
2021-10-31 14:57:28 +0000

    Upgrade curl/openssl dependecies to latest version

--- depends/packages/curl.mk
+++ depends/packages/curl.mk
@@ -1,15 +1,24 @@
 package=curl
-$(package)_version=7.77.0
+$(package)_version=7.79.1
 $(package)_download_path=https://curl.haxx.se/download/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=b0a3428acb60fa59044c4d0baae4e4fc09ae9af1d8a3aa84b2e3fbcd99841f77
+$(package)_sha256_hash=370b11201349816287fb0ccc995e420277fbfcaf76206e309b3f60f0eda090c2
 $(package)_dependencies=openssl zlib
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --with-ca-fallback
+$(package)_config_opts+=--disable-cookies
+$(package)_config_opts+=--disable-manual
+$(package)_config_opts+=--disable-unix-sockets
+$(package)_config_opts+=--disable-verbose
+$(package)_config_opts+=--disable-versioned-symbols
+$(package)_config_opts+=--enable-symbol-hiding
+$(package)_config_opts+=--without-librtmp
+$(package)_config_opts+=--disable-rtsp
+$(package)_config_opts+=--disable-alt-svc
 $(package)_config_opts_mingw32=--enable-sspi --without-ssl --with-nss --with-schannel
 $(package)_config_opts_darwin=--enable-sspi --without-ssl --with-secure-transport
-$(package)_config_opts_linux=--with-openssl
+$(package)_config_opts_linux=--with-ssl
 $(package)_cflags=-DCURL_STATICLIB
 $(package)_cxxflags=-std=c++11
 endef
@@ -26,4 +35,3 @@ endef
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
-
--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -1,9 +1,8 @@
 package=openssl
-$(package)_version=1.0.1k
+$(package)_version=1.1.1l
 $(package)_download_path=https://www.openssl.org/source
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=8f9faeaebad088e772f4ef5e38252d472be4d878c6b3a2718c10a4fcebe7a41c
-$(package)_patches=0001-Add-OpenSSL-termios-fix-for-musl-libc.patch
+$(package)_sha256_hash=0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1
 
 define $(package)_set_vars
 $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
@@ -15,29 +14,21 @@ $(package)_config_opts+=no-comp
 $(package)_config_opts+=no-dso
 $(package)_config_opts+=no-dtls1
 $(package)_config_opts+=no-ec_nistp_64_gcc_128
+$(package)_config_opts+=no-asm
+$(package)_config_opts+=no-engine
 $(package)_config_opts+=no-gost
-$(package)_config_opts+=no-gmp
-$(package)_config_opts+=no-heartbeats
 $(package)_config_opts+=no-idea
-$(package)_config_opts+=no-jpake
-$(package)_config_opts+=no-krb5
-$(package)_config_opts+=no-libunbound
 $(package)_config_opts+=no-md2
 $(package)_config_opts+=no-mdc2
 $(package)_config_opts+=no-rc4
 $(package)_config_opts+=no-rc5
 $(package)_config_opts+=no-rdrand
 $(package)_config_opts+=no-rfc3779
-$(package)_config_opts+=no-rsax
 $(package)_config_opts+=no-sctp
 $(package)_config_opts+=no-seed
-$(package)_config_opts+=no-sha0
 $(package)_config_opts+=no-shared
 $(package)_config_opts+=no-ssl-trace
-$(package)_config_opts+=no-ssl2
 $(package)_config_opts+=no-ssl3
-$(package)_config_opts+=no-static_engine
-$(package)_config_opts+=no-store
 $(package)_config_opts+=no-unit-test
 $(package)_config_opts+=no-weak-ssl-ciphers
 $(package)_config_opts+=no-whirlpool
@@ -60,12 +51,6 @@ $(package)_config_opts_x86_64_mingw32=mingw64
 $(package)_config_opts_i686_mingw32=mingw
 endef
 
-define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/0001-Add-OpenSSL-termios-fix-for-musl-libc.patch && \
-  sed -i.old "/define DATE/d" util/mkbuildinf.pl && \
-  sed -i.old "s|engines apps test|engines|" Makefile.org
-endef
-
 define $(package)_config_cmds
   ./Configure $($(package)_config_opts)
 endef
@@ -75,7 +60,7 @@ define $(package)_build_cmds
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) INSTALL_PREFIX=$($(package)_staging_dir) -j1 install_sw
+  $(MAKE) DESTDIR=$($(package)_staging_dir) -j1 install_sw
 endef
 
 define $(package)_postprocess_cmds
